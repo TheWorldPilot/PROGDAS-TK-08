97518118a260578e1af15194e9e86af88fd3e955
TheWorldPilot <beni.kenneth@gmail.com>
2026-04-30 16:37:40 +0700

    fix whatever's wrong with the db

--- calc.c
+++ calc.c
@@ -22,6 +22,7 @@ int main() {
 		result = input1+input2;
 		printf("Hasil penjumlahan : %.2f", result);
 		break;
+
 	//Subtraction
 	case 2:
 		printf("Input angka 1: ");
@@ -31,30 +32,8 @@ int main() {
 		result = input1-input2;
 		printf("Hasil penjumlahan : %.2f", result);
 		break;
+
 	//Multiplication
-
-    printf("Apa operasi yang ada ingin lakukan?"\n);
-    printf("1. Penjumlahan"\n);
-    printf("2. Pengurangan"\n);
-    printf("3. Perkalian"\n);
-    printf("4. Pembagian"\n);
-    printf("Apa operasi yang ada ingin lakukan? ");
-    scanf("%f", &n);
-
-    switch (n)
-    {
-        // Addition
-        case 1:
-            printf("Input angka 1: ");
-            scanf("%f", &input1);
-            printf("Input angka 2: ");
-            scanf("%f", &input1);
-            result = input1 + input2;
-            printf("Hasil penjumlahan : %.2f", result);
-            break;
-        // Subtraction
-
-        // Multiplication
         case 3:
             printf("Input angka 1:");
             scanf("%f", &input1);
@@ -64,7 +43,7 @@ int main() {
             printf("Hasil perkalian : %.2f", result);
             break;
 
-            // Division
+	// Division
 
             return 0;
     }
